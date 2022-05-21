codekandis/tiphy-session-integration

Initial commit.

tests/bootstrap.php
@@ -1,0 +1,18 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphySessionIntegration\Tests;
+
+use function error_reporting;
+use function ini_set;
+use const E_ALL;
+
+/**
+ * Represents the bootstrap script of the tests.
+ *
+ * @package codekandis/tiphy-session-integration
+ * @author  Christian Ramelow <info@codekandis.net>
+ */
+error_reporting( E_ALL );
+ini_set( 'display_errors', 'On' );
+ini_set( 'html_errors', 'Off' );
+
+require_once __DIR__ . '/../vendor/autoload.php';

codekandis/tiphy-ldap-integration

tests/bootstrap.php
@@ -1,0 +1,18 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphyLdapIntegration\Tests;
+
+use function error_reporting;
+use function ini_set;
+use const E_ALL;
+
+/**
+ * Represents the bootstrap script of the tests.
+ *
+ * @package codekandis/tiphy-ldap-integration
+ * @author  Christian Ramelow <info@codekandis.net>
+ */
+error_reporting( E_ALL );
+ini_set( 'display_errors', 'On' );
+ini_set( 'html_errors', 'Off' );
+
+require_once __DIR__ . '/../vendor/autoload.php';